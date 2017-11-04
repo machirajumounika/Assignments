fb80bac7b1b8d2e484a65a21f09ee7d7be659ca4
Mounika Machiraju <mmounika92@gmail.com>
2017-11-04 13:21:22 -0400

    Create a reusable piece of code that will print name of the user using Void return type.

---
+++ src/com/assignment/assignment1/Assignment.java
@@ -0,0 +1,20 @@
+package com.assignment.assignment1;
+
+public class Assignment {
+
+	public void printName(String name) {
+		System.out.println("You have entered your name as " + name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Assignment assignemnt = new Assignment();
+		assignemnt.printName("Ram");
+		assignemnt.printName("Sham");
+		assignemnt.printName("Steve");
+		assignemnt.printName("Tim");
+
+	}
+
+}
