3104c96953d602663966c078eb5d251733c9af17
Srinivas Gudipati <srinivasgudipati321@gmail.com>
2017-11-04 13:35:14 -0400

    Adding New Class

---
+++ src/com/assignment/assignment1/Program2.java
@@ -0,0 +1,22 @@
+/**
+* The  program implements an application that
+* simply displays Month to the standard output.
+*
+* @author  Mounika Machiraju
+* @version 1.0
+* @since   2017-11-04 
+*/
+
+package com.assignment.assignment1;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
